414e038ef2e4fef5df774e88933d45d75ab53ed0
TCole79 <tristanleighcole@googlemail.com>
2021-12-08 21:28:51 +0000

    Added code to test the register template as per task 1 in compass on W3D3

--- express_server.js
+++ express_server.js
@@ -1,20 +1,25 @@
 ////---- REQUIRED ----////
 const express = require("express");
-const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080;
-
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const { reset } = require("nodemon"); // is this being used as its dull?
+// morgan? const morgan = require('morgan');
+
+
+////---- MIDDLEWARE ----////
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+// morgan? app.set(morgan('dev'));
+
 
 ////---- LISTEN ----////
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-////---- MIDDLEWARE ----////
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 
 
 ////---- URL DATABASE ----////
@@ -23,15 +28,125 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
-////---- ROUTES ----////
 
+
+////---- GENERATE RANDOM STRING ----////
 // this code generates a random string up to 6 characters long
-const generateRandomString = function () {
+const generateRandomString = function() {
   return Math.random()
     .toString(36)
     .replace(/[^a-z]+/g, "")
     .substr(0, 6);
 };
+
+
+////---- CREATE USERS OBJECT ----////
+const users = {
+  userRandomID: {
+    id: "testUser1",
+    email: "user1@example.com",
+    password: "1234",
+  },
+  user2RandomID: {
+    id: "testUser2",
+    email: "user2@example.com",
+    password: "4321",
+  },
+};
+// class example below
+// const findUserByEmail = (email) => {
+//   for (const userID in users) {
+//     const user = users[userID];
+//     if (user.email === email) {
+//       return user;
+//     }
+//   }
+//   return null;
+// };
+
+
+////---- ROUTES ----////
+
+
+////---- NEW USER REGISTRATION HANDLER ----////
+
+
+app.post("/register"), (req, res) => {
+
+};
+
+app.get("/register", (req, res) => {
+  // const email = req.body.email;
+  // const password = req.body.password;
+
+  // if (!email || !password) {
+  //   return res.status(400).send("email and password cannot be blank");
+  // }
+
+  // const user = findUserByEmail(email);
+  // if (user) {
+  //   return res.status(400).send("User already exists with that email.");
+  // }
+
+  //const id = generateRandomString();
+
+  // users[id] = {
+  //   id: id,
+  //   email: email,
+  //   password: password
+  // };
+
+  // tinyapp will have us login them in immediately on registration
+
+  // const userLoginID = something ["userLoginID"]?
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  res.render("register", templateVars);
+  //res.redirect("/urls");
+});
+
+
+////---- USER LOGIN ----////
+// example from the lecture
+// app.post("/login"), (req, res) => {
+//   const email = req.body.email;
+//   const password = req.body.password;
+
+//   if (!email || !password) {
+//     return res.status(400).send("email and password cannot be blank");
+//   }
+
+//   const user = findUserByEmail(email);
+
+//   if (!user) {
+//     return res.status(400).send("A user with that email does not exist.");
+//   }
+//   if (user.password !== password) {
+//     return res.status(400).send("Password does not match.");
+//   }
+
+//   res.cookie('user_id', user.id); // change this to reflect my example
+//   res.redirect('/secrets');
+
+//   app.get('/secrets', (req, res) => {
+//     const userId = req.cookies.user_ID; // change this user bit to reflect mine
+
+//     if (!userID) {
+//       return res.status(400).send("You are not authorized to be here."); // change user bit
+//     }
+
+//     const user = users[userId]; // this should match the ejs file in croc code
+//     const templateVars = {
+//       user
+//     }
+
+//     //if (!user)
+//   });
+// };
+
+// app.get("/login"), (req, res) => {
+
+// };
+
 
 // this code '/' requests the homepage
 app.get("/", (req, res) => {
@@ -52,14 +167,12 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase,
-    username: req.cookies["username"], }; // added this in order to render templates properly.
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] }; // added this in order to render templates properly.
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = { urls: urlDatabase,
-    username: req.cookies["username"], };
+  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
   res.render("urls_new", templateVars);
 });
 
@@ -71,11 +184,6 @@ app.get("/urls/:shortURL", (req, res) => {
   };
   console.log("test 2 ", templateVars);
   res.render("urls_show", templateVars);
-});
-
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.EditField;
-  res.redirect("/urls");
 });
 
 ////---- DELETE URL ----////
@@ -91,7 +199,7 @@ app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   let longURL = req.body.longURL;
   urlDatabase[shortURL] = longURL;
-  
+
   res.redirect(`/urls/${shortURL}`);
 });
 
@@ -105,6 +213,6 @@ app.post("/login", (req, res) => {
 
 ////---- LOGOUT ----////
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie("username");
   res.redirect("/urls");
 });
