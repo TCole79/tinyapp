2e8f617714b1d25e9004673800177805dc6b9cbc
TCole79 <tristanleighcole@googlemail.com>
2021-12-10 21:59:48 +0000

    changed over to encrypted cookies and tested, all working

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-////---- REQUIRE ----////
+////---- REQUIRE START ----////
 const express = require("express");
 const app = express();
 const PORT = 8080;
@@ -12,25 +12,29 @@ const {
   findUserByEmail,
   urlsForUser,
 } = require("./helpers");
+////---- REQUIRE END ----////
 
 
-////---- MIDDLEWARE ----////
+////---- MIDDLEWARE START ----////
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-// app.use(cookieSession ( {
-//   name: "session",
-//   key: [cookie, session],
-// }));
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["cookie", "session"]
+  }));
+////---- MIDDLEWARE END ----////
 
 
-////---- LISTEN TO PORT MESSAGE----////
+////---- LISTEN TO PORT MESSAGE START ----////
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+////---- LISTEN TO PORT MESSAGE END ----////
 
 
-////---- URL DATABASE ----////
+////---- URL DATABASE START ----////
 const urlDatabase = {
   b6UTxQ: {
     longURL: "http://lighthouselabs.ca",
@@ -41,13 +45,14 @@ const urlDatabase = {
     userID: "aJ48lW"
   },
 };
+////---- URL DATABASE END ----////
 
 
 ////---- ROUTES ----////
 
 
 
-////---- USERS DATABASE ----////
+////---- USERS DATABASE START ----////
 const users = {
   "testUser1": {
     id: "testUser1",
@@ -60,16 +65,22 @@ const users = {
     password: "$2a$10$YzUcDniy0G51xW0Uq6imMerjC2rfouxsWsgEa0SQiqHXWO5A78BNO",
   },
 };
+////---- USERS DATABASE END ----////
 
 
 
-////---- NEW USER REGISTRATION HANDLER ----////
+////---- NEW USER REGISTRATION HANDLER START ----////
 app.get("/register", (req, res) => {
-  const templateVars = {
-    urls: urlDatabase,
-    user: users[req.cookies["user_id"]],
-  };
-  res.render("register", templateVars);
+  const userID = req.session["user_id"];
+  if (!userID) {
+    const templateVars = {
+      urls: urlDatabase, // is this still needed?
+      user: users[req.session["user_id"]], // changed session from cookies
+    };
+    res.render("register", templateVars);
+  } else {
+    res.redirect("/urls");
+  }
 });
 // GET /register - requirements
 //     if user is logged in:
@@ -92,19 +103,19 @@ app.post("/register", (req, res) => {
   const user = findUserByEmail(users, email);
   if (user) {
     return res.status(403).send("User already exists with that email.");
+  } else {
+    const id = generateRandomString();
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+    users[id] = {
+      id: id,
+      email: email,
+      password: hashedPassword
+    };
+    //res.cookie("user_id", id);
+    req.session.user_id = id; // added this in - why is it saying camelcase?
+    res.redirect("/urls");
   }
-
-  const id = generateRandomString();
-
-  users[id] = {
-    id: id,
-    email: email,
-    password: hashedPassword
-    //password: bcrypt.hashsync(password, salt), // this code for adding the encryption
-  };
-
-  res.cookie("user_id", id);
-  res.redirect("/urls");
 });
 // POST /register - requirements
 //     if email or password are empty:
@@ -117,35 +128,30 @@ app.post("/register", (req, res) => {
 //         sets a cookie
 //         redirects to /urls
 
+////---- NEW USER REGISTRATION HANDLER END ----////
+
 
 // this code '/' requests the homepage
 app.get("/", (req, res) => {
-  // const something for the id here?
-  // if (user) {
-  //   res.redirect("/urls");
-  // } else {
-  //   res.redirect("/login");
-  // }
-  //res.send("Hello!\n");
-});
-// GET / - requirements
-//     if user is logged in:
-//         (Minor) redirect to /urls
-//     if user is not logged in:
-//         (Minor) redirect to /login
-
-
-app.get("/urls", (req, res) => {
-  const userID = req.cookies["user_id"]; // should this be something else?
-  console.log('test 1 ', userID);
+  const userID = req.session["user_id"];
   if (!userID) {
-    res.status(401).send(`You must be logged in to view short URLs you own. <a href="/login">Log Into Your Account </a>`); // adding a <a> tag here allows you to go back to the login page
+    res.redirect("/login");
+  }
+  res.redirect("/urls");
+});
+
+
+////---- MAIN TINY APP PAGE START ----////
+app.get("/urls", (req, res) => {
+  const userID = req.session["user_id"]; // changed session from cookies
+  if (!userID) {
+    res.status(401).send(`You must be logged in to create, view, or edit short URLs. <a href="/login">Log Into Your Account </a>`); // adding a <a> tag here allows you to go back to the login page
     return;
   }
   const userURLS = urlsForUser(urlDatabase, userID);
   const templateVars = {
-    urls: userURLS,
-    user: users[req.cookies["user_id"]],
+    urls: userURLS, // is this needed?
+    user: users[req.session["user_id"]], // changed session from cookies
   };
   res.render("urls_index", templateVars);
 });
@@ -166,18 +172,20 @@ app.get("/urls", (req, res) => {
 //         returns HTML with a relevant error message
 
 
+////---- ADDING NEW URLS START ----////
 app.get("/urls/new", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session["user_id"]; // changed session from cookies
   if (!userID) {
     res.redirect("/login");
-  } else {
-    const templateVars = {
-      user: users[req.cookies["user_id"]], // had to add this back in to fix 'user not defined' error
-      //users,
-      userID,
-    };
-    res.render("urls_new", templateVars);
   }
+  
+  const templateVars = {
+    user: users[req.session["user_id"]], // changed session from cookies
+    //users, is this needed?
+    userID,
+  };
+  res.render("urls_new", templateVars);
+  
 });
 // GET /urls/new - requirments
 //     if user is logged in:
@@ -189,9 +197,29 @@ app.get("/urls/new", (req, res) => {
 //     if user is not logged in:
 //         redirects to the /login page
 
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = {
+    shortURL: shortURL, // is this needed?
+    longURL: longURL,
+    userID: req.session["user_id"], // changed session from cookies
+  };
+  res.redirect(`/urls/${shortURL}`);
+});
+// POST /urls - requirements
+//     if user is logged in:
+//         generates a short URL, saves it, and associates it with the user
+//         redirects to /urls/:id, where :id matches the ID of the newly saved URL
+//     if user is not logged in:
+//         (Minor) returns HTML with a relevant error message
+
+////---- ADDING NEW URLS END----////
+
 
 app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL].longURL; // added longURL on to the end to test the new structure
+  const longURL = urlDatabase[req.params.shortURL].longURL; // added longURL on to the end to test the new structure
+  // this is the bit where I need to add http:// onto all my long urls in order to make them actually work (if entered in shortform ie google.com) - how to do this?
   res.redirect(longURL);
 });
 // GET /u/:id (shortURL) - requirements
@@ -201,19 +229,17 @@ app.get("/u/:shortURL", (req, res) => {
 //         (Minor) returns HTML with a relevant error message
 
 
+////---- EDITING URLS START ----////
 app.get("/urls/:shortURL", (req, res) => {
-  const userID = req.cookies["user_id"];
-  if (!userID) {
-    res.status(401).send(`You must be logged in to edit short URLs you own. <a href="/login">Log Into Your Account </a>`);
-  }
+  const userID = req.session["user_id"]; // changed session from cookies
+  const urlRecord = urlDatabase[req.params.shortURL];
   // in my url database, each object has a userID associated. If that does not = a current userID then throw an error
-  // the following code borks everything - Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client
-  // if (urlDatabase[userID] !== userID) {
-  //   res.status(401).send(`You must be logged in to edit short URLs you own. <a href="/login">Log Into Your Account </a>`);
-  // }
-  
+  if (!userID || userID !== urlRecord.userID) { // is this correct?
+    res.status(401).send(`You must be logged in to edit short URLs you own. Please try again. <a href="/login">Log Into Your Account </a>`);
+    return;
+  }
   const templateVars = {
-    user: users[req.cookies["user_id"]],
+    user: users[req.session["user_id"]], // changed session from cookies
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL].longURL, // added longURL on to the end to test the new structure
   };
@@ -237,33 +263,17 @@ app.get("/urls/:shortURL", (req, res) => {
 //     if user is logged it but does not own the URL with the given ID:
 //         returns HTML with a relevant error message
 
-
-
-////---- ADDING NEW URLS ----////
-app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
-  let longURL = req.body.longURL;
-  urlDatabase[shortURL] = {
-    longURL: longURL,
-    userID: req.cookies["user_id"],
-  };
-  res.redirect(`/urls/${shortURL}`);
-});
-// POST /urls - requirements
-//     if user is logged in:
-//         generates a short URL, saves it, and associates it with the user
-//         redirects to /urls/:id, where :id matches the ID of the newly saved URL
-//     if user is not logged in:
-//         (Minor) returns HTML with a relevant error message
-
-
-
 app.post("/urls/:shortURL", (req, res) => {
+  const userID = req.session["user_id"]; // changed session from cookies
+  const urlRecord = urlDatabase[req.params.shortURL];
+  // in my url database, each object has a userID associated. If that does not = a current userID then throw an error
+  if (!userID || userID !== urlRecord.userID) { // is this correct?
+    res.status(401).send(`You must be logged in to edit short URLs you own. Please try again. <a href="/login">Log Into Your Account </a>`);
+    return;
+  }
   urlDatabase[req.params.shortURL].longURL = req.body.EditField;
-  
   res.redirect("/urls");
 });
-
 // POST /urls/:id (shortURL) - requirements
 //     if user is logged in and owns the URL for the given ID:
 //         updates the URL
@@ -273,15 +283,18 @@ app.post("/urls/:shortURL", (req, res) => {
 //     if user is logged it but does not own the URL for the given ID:
 //         (Minor) returns HTML with a relevant error message
 
+////---- EDITING URLS END----////
 
-////---- DELETE URLS ----////
+
+////---- DELETE URLS START ----////
 app.post("/urls/:shortURL/delete", (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session["user_id"]; // changed session from cookies
+  const urlRecord = urlDatabase[req.params.shortURL];
   const idToDelete = req.params.shortURL;
 
-  if (!userID) {
+  if (!userID || userID !== urlRecord.userID) {
     res.status(401).send(`You must be logged in to delete short URLs you own. <a href="/login">Log Into Your Account </a>`);
-    //res.redirect("/login");
+    return; // is this needed?
   } else {
     delete urlDatabase[idToDelete];
   }
@@ -289,53 +302,28 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 // POST /urls/:id/delete (shortURL) - requirements
 // if user is logged in and owns the URL for the given ID:
-
 //     deletes the URL
 //     redirects to /urls
-
 // if user is not logged in:
 //     (Minor) returns HTML with a relevant error message
-
 // if user is logged it but does not own the URL for the given ID:
 //     (Minor) returns HTML with a relevant error message
 
+////---- DELETE URLS END ----////
 
 
-////---- USER LOGIN HANDLER----////
 
-app.post("/login", (req, res) => {
-  let email = req.body.email;
-  let password = (req.body.password); // changed this
-  console.log('test users ', users);
-  //let userEmail = findUserByEmail(users, email); // is this correct? it users the helper function from earlier
-
-  if (!email || !password) {
-    return res.status(403).send("Email and password cannot be blank.");
+////---- USER LOGIN HANDLER START ----////
+app.get("/login", (req, res) => {
+  const userID = req.session["user_id"];
+  if (!userID) {
+    const templateVars = {
+      user: users[req.session["user_id"]], // changed to session from cookies
+    };
+    res.render("login", templateVars);
+  } else {
+    res.redirect("/urls");
   }
-
-  const user = findUserByEmail(users, email);
-  console.log('test user ', user);
-  if (!user) {
-    return res.status(403).send("A user with that email does not exist.");
-  }
-  if (!bcrypt.compareSync(password, user.password)) {
-    return res.status(403).send("User email or password does not match.");
-  }
-  res.cookie('user_id', user.id);
-  res.redirect("/urls");
-});
-// POST /login - requirements
-//     if email and password params match an existing user:
-//         sets a cookie
-//         redirects to /urls
-//     if email and password params don't match an existing user:
-//         returns HTML with a relevant error message
-
-app.get('/login', (req, res) => {
-  const templateVars = {
-    user: users[req.cookies["user_id"]],
-  };
-  res.render('login', templateVars);
 });
 // GET /login - requirements
 //     if user is logged in:
@@ -346,15 +334,41 @@ app.get('/login', (req, res) => {
 //             input fields for email and password
 //             submit button that makes a POST request to /login
 
+app.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password; // removed () from around the variable
+  const user = findUserByEmail(users, email); // is this correct? it users the helper function from earlier
 
-////---- USER LOGOUT ----////
+  if (!email || !password) {
+    return res.status(403).send(`Email and password cannot be blank. Please try again. <a href="/login">Log Into Your Account </a>`);
+  }
 
+  //const user = findUserByEmail(users, email);
+  if (!user) {
+    return res.status(403).send(`A user with that email does not exist. Please try again. <a href="/login">Log Into Your Account </a>`);
+  }
+  if (!bcrypt.compareSync(password, user.password)) {
+    return res.status(403).send(`User email or password does not match. Please try again. <a href="/login">Log Into Your Account </a>`);
+  }
+  //res.cookie('user_id', user.id);
+  req.session.user_id = user.id;
+  res.redirect("/urls");
+});
+// POST /login - requirements
+//     if email and password params match an existing user:
+//         sets a cookie
+//         redirects to /urls
+//     if email and password params don't match an existing user:
+//         returns HTML with a relevant error message
+
+////---- USER LOGIN HANDLER END ----////
+
+
+////---- USER LOGOUT START ----////
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
-  //req.session.email = null // this is one way to use, after implementing encryption
+  //res.clearCookie("user_id");
+  req.session = null; // this is one way to use, after implementing encryption
   //delete req.session.email; // this is one way to use, after implementing encryption
   res.redirect("/urls");
 });
-// POST /logout - requirements
-//     deletes cookie
-//     redirects to /urls
+////---- USER LOGOUT END ----////
